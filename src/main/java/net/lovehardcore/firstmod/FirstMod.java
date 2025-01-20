edee2826b73223d3bfb248cf7e930ad584c496b4
Hidden <quan170113@gmail.com>
2025-01-20 18:13:15 +0700

    Added the following: -Raw pink garnet -Pink garnet

--- src/main/java/net/lovehardcore/firstmod/FirstMod.java
+++ src/main/java/net/lovehardcore/firstmod/FirstMod.java
@@ -2,6 +2,7 @@ package net.lovehardcore.firstmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lovehardcore.firstmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +13,6 @@ public class FirstMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
