3e9feab330538666ba679143591a36261b797429
Hidden <quan170113@gmail.com>
2025-01-21 21:48:27 +0700

    Added the following: - 2 new Item Groups   + Pink Garnet Items   + Oink Garnet Blocks Previous additions: - Pink Garnet Block - Raw Pink Garnet Block - Pink Garnet (item) - Raw Pink Garnet (item) - Source link

--- src/main/java/net/lovehardcore/firstmod/FirstMod.java
+++ src/main/java/net/lovehardcore/firstmod/FirstMod.java
@@ -3,6 +3,7 @@ package net.lovehardcore.firstmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.lovehardcore.firstmod.block.ModBlocks;
+import net.lovehardcore.firstmod.item.ModItemgroups;
 import net.lovehardcore.firstmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,7 @@ public class FirstMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemgroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
